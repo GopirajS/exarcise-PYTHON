316765dfcbe9d9e20ed03447e52b337f1cba49c1
GopirajS <81741824+GopirajS@users.noreply.github.com>
2021-10-01 16:23:46 +0530

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,49 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 1
+1
+>>> 1+2
+3
+>>> 2+3
+5
+>>> 8+_
+13
+>>> 8+_
+21
+>>> 8+_
+29
+>>> 8+_
+37
+>>> 8+_
+45
+>>> 50-2*2
+46
+>>> (20-2+3)/3
+7.0
+>>> 3-2
+1
+>>> 3-4
+-1
+>>> 2*3
+6
+>>> 6/4
+1.5
+>>> 6/2
+3.0
+
+>>> 20//2 #floor divition
+10
+
+>>> 20%2 #remainder to the divtion
+0
+>>> 20%3 #remainder to the divtion
+2
+>>> 3**7 #3 to the power of 7
+2187
+>>> 
+>>> 
+>>> width=12
+>>> height=23
+>>> width*height
+276
+>>> 
