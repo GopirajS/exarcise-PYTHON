03fdccf70297e9a2437a799a0c8f11e80ed47df1
GopirajS <81741824+GopirajS@users.noreply.github.com>
2021-10-13 12:30:43 +0530

    IF statement

---
+++ IF_statement.py
@@ -0,0 +1,166 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> # python if ,elif , else condition
+>>> 
+>>> price=50
+>>> if price < 100 :
+	print('prince lessthen 100')
+
+	
+prince lessthen 100
+>>> price=50
+>>> quantity=5
+>>> 
+>>> if privce*quantity <500:
+	print('total price is less then 500')
+
+	
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    if privce*quantity <500:
+NameError: name 'privce' is not defined
+>>> if price*quantity <500:
+	print('total price is less then 500')
+
+	
+total price is less then 500
+>>> 
+>>> if True:
+	print("contition true ")
+
+	
+contition true 
+>>> if 1:
+	print('1 is true \n so if condition is run')
+
+	
+1 is true 
+ so if condition is run
+>>> 
+>>> if True:
+	print('first condition is true')
+	if True:
+		print('second contition is true')
+
+		
+first condition is true
+second contition is true
+>>> 
+>>> 
+>>> 
+>>> if 12<2:
+	print('if condiotn is false')
+else:
+	print('if codition is false \nso else statment is run')
+
+	
+if codition is false 
+so else statment is run
+>>> 
+>>> 
+>>> if False:
+	print('false')
+else:
+	print('hellow world')
+
+	
+hellow world
+
+
+>>> price=500
+>>> qantity=5
+
+>>> if price*qantity < 10000:
+	print('total price is ',price*qantity)
+	print('price',price)
+	print('quantity',quantity)
+
+	
+total price is  2500
+price 500
+quantity 5
+
+
+
+
+>>> price=100
+>>> if price ==100:
+	print('price quale to 100')
+
+	
+price quale to 100
+>>> if price <=100:
+	print('price is lessthen are equal of 100')
+
+	
+price is lessthen are equal of 100
+>>> if price >=100:
+	print('price is morethen are quale to 100')
+
+	
+price is morethen are quale to 100
+
+
+>>>#--------------------------------------------------------
+>>> # else condition
+>>> price = 50
+>>> if price > 100:
+	print('price is greater then of 100')
+else:
+	print('price is less then of 100')
+
+	
+price is less then of 100
+
+>>> #-----------------------------------------------------------------
+
+>>> # elif condition
+>>> price=50
+>>> 
+>>> if price==10:
+	print('price is quale to 10')
+elif price < 20:
+	print('price is less then of 20')
+elif price < 30:
+	print('price is less then of 30')
+elif price < 50:
+	print('prince is less then are quale 50')
+else :
+	print('belong contition are false  \n so else steement run')
+
+	
+belong contition are false  
+ so else steement run
+>>> if price==10:
+	print('price is quale to 10')
+elif price < 20:
+	print('price is less then of 20')
+elif price < 30:
+	print('price is less then of 30')
+elif price <= 50:
+	print('prince is less then are quale 50')
+else :
+	print('belong contition are false  \n so else steement run')
+
+	
+prince is less then are quale 50
+>>> 
+>>> 
+>>> 
+>>> if True:
+	if False:
+		print('seconf if true')
+	elif False:
+		print('seconf if first elif True')
+	elif True :
+		print('seconf if second elif true ')
+	else :
+		print('else')
+elif False:
+	print('first if elif ')
+else:
+	print('gopi')
+
+	
+seconf if second elif true 
+>>> 
