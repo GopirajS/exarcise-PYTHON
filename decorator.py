24194782a94c3ee208bf29cb93ab1c03c6d0cc3d
GopirajS <81741824+GopirajS@users.noreply.github.com>
2021-11-16 19:47:18 +0530

    decorator

--- decorator.py
+++ decorator.py
@@ -484,3 +484,318 @@ def print_str():
 'HELLOW WORLD'
 
 
+
+#==================================================
+
+>>> import math
+
+>>> math.factorial(4)
+24
+
+>>> def factorial(x):
+	print(x*x)
+
+	
+>>> factorial(4)
+16
+
+>>> math.factorial(5)
+120
+
+
+>>> from math import factorial
+
+>>> factorial(5)
+120
+
+>>> def factorial(x):
+	print(x*x)
+
+	
+>>> factorial(5)
+25
+
+>>> math.factorial(5)
+120
+
+>>> #----------------------
+
+
+>>> def fuction(x):
+	y=123
+	print(x+y)
+
+	
+>>> fuction(4)
+127
+
+
+>>> def fuction():
+	s='gopi'
+	print(s)
+
+	
+>>> fuction()
+gopi
+
+
+>>> name='gopi raj'
+
+>>> def outer():
+	name='sathis kuamr'
+	def inner():
+		name='indumathi'
+		print(name)
+
+		
+
+>>> outer()
+
+
+>>> def outer():
+	name='sathis kuamr'
+	def inner():
+		name='indumathi'
+		print(name)
+	inner()
+
+	
+>>> outer()
+indumathi
+
+>>> def outer():
+	name='sathis kuamr'
+	def inner():
+		name='indumathi'
+		print(name)
+	return inner()
+
+>>> outer()
+indumathi
+
+>>> s=outer()
+indumathi
+
+>>> s
+
+>>> def outer():
+	name='sathis kuamr'
+	def inner():
+		name='indumathi'
+		print(name)
+	return inner
+
+>>> s=outer()
+
+>>> s
+<function outer.<locals>.inner at 0x000001F14CC0FB80>
+
+>>> s()
+indumathi
+
+
+>>> def outer():
+	name='sathis kuamr'
+	def inner():
+		#name='indumathi'
+		print(name)
+	return inner
+
+>>> s=outer()()
+sathis kuamr
+
+
+>>> s
+
+>>> def outer():
+	#name='sathis kuamr'
+	def inner():
+		#name='indumathi'
+		print(name)
+	return inner
+
+>>> s=outer()
+
+
+>>> s()
+gopi raj
+
+>>> s.__name__
+'inner'
+
+>>> #-------------------------------------
+
+>>> def outer(fuc):
+	def inner():
+		s=fuc()
+		return s.upper()
+	return inner
+
+>>> @outer
+def print_str():
+	s=input('Enter a string :')
+	return s
+
+
+>>> print_str()
+Enter a string :hellow world
+'HELLOW WORLD'
+
+
+>>> def outer(fuc):
+	def inner():
+		s=fuc()
+		return s.upper()
+	print('before calling inner fuction')
+	return inner
+
+
+>>> @outer
+def print_str():
+	s=input('Enter a string :')
+	return s
+
+before calling inner fuction
+
+
+>>> print_str()
+Enter a string :'helllow'
+"'HELLLOW'"
+
+
+>>> def outer(fuc):
+	def inner():
+		s=fuc()
+		print('before calling')
+		return s.upper()
+	return inner
+
+
+
+>>> @outer
+def print_str():
+	s=input('Enter a string :')
+	return s
+
+
+>>> print_str()
+Enter a string :hellow world
+before calling
+'HELLOW WORLD'
+
+
+>>> def upper(fn):
+	s=fn()
+	return s.upper()
+
+	
+
+>>> @upper
+def print_str():
+	s=input('Enter a string :')
+	return s
+
+Enter a string :
+
+
+>>> def s(fcn):
+	def inner():
+		f=fcn()
+		return f.upper()
+	return inner
+
+
+>>> @s
+def print_str():
+	s=input("enter a string :")
+	return s
+
+
+>>> print_str()
+enter a string :gopiraj
+'GOPIRAJ'
+
+
+
+>>> def s(fcn):
+	f=fcn()
+	return f.upper()
+
+
+>>> @s
+def print_str():
+	return 'hellow world'
+
+
+>>> print_str
+'HELLOW WORLD'
+
+
+
+>>> type(print_str)
+<class 'str'>
+
+
+>>> print_str
+'HELLOW WORLD'
+
+
+>>> def s(fcn):
+	f=fcn()
+	return f.upper()
+
+>>> def print_str():
+	return 'hellow world'
+
+
+>>> s(print_str)
+'HELLOW WORLD'
+
+>>> print_str()
+'hellow world'
+
+
+>>> def s(fcn):
+	def inner():
+		f=fcn()
+		return f.upper()
+	return inner
+
+
+>>> @s
+def d():
+	return 'hellow world'
+
+>>> d
+<function s.<locals>.inner at 0x000001F14CC2B040>
+
+>>> @s
+def d():
+	n=input('Enter a string')
+	return
+
+>>> d()
+Enter a string hellow
+Traceback (most recent call last):
+  File "<pyshell#167>", line 1, in <module>
+    d()
+  File "<pyshell#157>", line 4, in inner
+    return f.upper()
+AttributeError: 'NoneType' object has no attribute 'upper'
+
+>>> @s
+def d():
+	n=input('Enter a string')
+	return n
+
+>>> d
+<function s.<locals>.inner at 0x000001F14CC2B280>
+
+>>> d()
+Enter a string hellow
+' HELLOW'
+
+>>> d()
+Enter a stringgopiraj
+'GOPIRAJ'
+
