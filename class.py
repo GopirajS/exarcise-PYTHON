bb1b6644993f710cc322fa8896dcb12845db2771
GopirajS <81741824+GopirajS@users.noreply.github.com>
2021-11-25 17:19:28 +0530

    class

--- class.py
+++ class.py
@@ -609,3 +609,370 @@ Traceback (most recent call last):
     mycls
 NameError: name 'mycls' is not defined
 
+
+	
+>>>#==========================================================================
+
+
+
+
+>>> class my_class():
+	def __init__(self,name):
+		self.name=name
+	def msg(self):
+		return  'Hellow Mr '+ self.name
+
+	
+>>> s=my_class('Gpi')
+
+>>> s.name
+'Gpi'
+
+>>> s.msg()
+'Hellow Mr Gpi'
+
+
+>>> class dog():
+	animal='dog'
+	def __init__(self,breed,color):
+		self.breed=breed
+		self.color=color
+
+
+>>> gimmy=dog('pug','brown')
+
+>>> gimmy.animal
+'dog'
+
+>>> gimmy.breed
+'pug'
+
+>>> gimmy.color
+'brown'
+
+>>> tommy=dog('bulldog','black')
+
+>>> tommy.color
+'black'
+
+>>> tommy.breed
+'bulldog'
+
+>>> tommy.animal
+'dog'
+
+>>> class greed():
+	def __init__(self):
+		self.geek='hellow world'
+	def print_msg(self):
+		print(self.geek)
+
+		
+>>> s=greed()
+
+>>> s.geek
+'hellow world'
+
+>>> s.print_msg()
+hellow world
+
+>>> s.geek='hellow world in python'
+
+>>> s.print_msg()
+hellow world in python
+
+
+>>> class calculate():
+	def __init__(self,f,s):
+		self.f=f
+		self.s=s
+	def print__msg(self):
+		print('first number:',self.f)
+		print('second number',self.s)
+		print('answer is :',self.ans)
+	def calculate(self):
+		self.ans=self.f+self.s
+
+		
+
+>>> s=calculate(2,3)
+
+>>> s.f
+2
+
+>>> s.s
+3
+
+>>> s.calculate()
+
+>>> s.print__msg()
+first number: 2
+second number 3
+answer is : 5
+
+>>> s.f=1
+
+>>> s.s=2
+
+>>> s.print__msg()
+first number: 1
+second number 2
+answer is : 5
+
+>>> s.calculate()
+
+>>> s.print__msg()
+first number: 1
+second number 2
+answer is : 3
+
+>>> d=calculate(1,2)
+
+>>> d.f
+1
+
+>>> d.s
+2
+
+
+>>> class s:
+	first=0
+	second=0
+	def __init__(self,f,s):
+		self.first=f
+		self.second=s
+	def print_msg(self):
+		print('first number',self.first)
+		print('second number',self.second)
+
+		
+>>> s=s(1,2)
+
+>>> s.first
+1
+
+>>> s.second
+2
+
+>>> s.print_msg()
+first number 1
+second number 2
+
+
+>>> class s:
+	fir=0
+	sec=0
+	def __init__(self,f,s):
+		self.first=f
+		self.second=s
+	def print_msg(self):
+		print('first number',self.first)
+		print('second number',self.second)
+
+		
+
+>>> s=s(1,2)
+
+>>> s.first
+1
+
+>>> s.second
+2
+
+>>> s.fir
+0
+
+>>> s.sec
+0
+
+>>> s.print_msg()
+first number 1
+second number 2
+
+>>> class s:
+	first=0
+	second=0
+	def __init__(self,f,s):
+		self.first=f
+		self.second=s
+	def print_msg(self):
+		print('first number',self.first)
+		print('second number',self.second)
+
+		
+
+>>> s=s(1,2)
+
+
+>>> s.print_msg()
+first number 1
+second number 2
+
+>>> s.second=3
+
+>>> s.print_msg()
+first number 1
+second number 3
+
+>>> class destro():
+	def __init__(self,name):
+		self.name=name
+		print('Destructor')
+	def __del__(self):
+		print('dectructor is called..')
+		print('deleted reference of obj of class')
+
+		
+
+>>> s=destro('Gopi')
+Destructor
+
+>>> s.name
+'Gopi'
+
+
+>>> del s
+dectructor is called..
+deleted reference of obj of class
+
+
+
+>>> del destro
+
+
+>>> class destro():
+	def __init__(self,name):
+		self.name=name
+		print('Destructor')
+	def __del__(self):
+		print('dectructor is called..')
+		print('deleted reference of obj of class')
+
+		
+
+>>> destro
+<class '__main__.destro'>
+
+
+>>> def creat_obj():
+	print('Making object for class')
+	obj=destro('Gopi')
+	print('function ended..')
+	return obj
+
+
+>>> s=destro('Gopi')
+Destructor
+
+>>> s
+<__main__.destro object at 0x000001D2D8C040D0>
+
+>>> s=creat_obj()
+Making object for class
+Destructor
+function ended..
+
+>>> s
+dectructor is called..
+deleted reference of obj of class
+<__main__.destro object at 0x000001D2D8C9EC70>
+
+>>> s
+<__main__.destro object at 0x000001D2D8C9EC70>
+
+>>> def fuction():
+	print('imm from function')
+
+	
+>>> s=fuction()
+imm from function
+
+>>> s
+
+>>> type(s)
+dectructor is called..
+deleted reference of obj of class
+<class 'NoneType'>
+
+>>> s
+
+>>> type(s)
+<class 'NoneType'>
+
+>>> def fuction():
+	print('im from fuction')
+	s=class_my()
+	return s
+
+
+>>> class class_my():
+	def __init__(self):
+		print('im from constructor in class')
+
+		
+>>> s=class_my()
+im from constructor in class
+
+>>> d=fuction()
+im from fuction
+im from constructor in class
+
+>>> def fuction():
+	print('im from fuction')
+	s=class_my()
+	d='hello world'
+	return d
+
+>>> s=class_my()
+im from constructor in class
+
+>>> obj=fuction()
+im from fuction
+im from constructor in class
+
+>>> obj
+'hello world'
+
+>>> def fuction():
+	print('im from fuction')
+	s=class_my()
+	return s
+
+
+>>> s=class_my()
+im from constructor in class
+
+>>> obj=fuction()
+im from fuction
+im from constructor in class
+
+>>> obj
+<__main__.class_my object at 0x000001D2D8C9ECA0>
+
+>>> class my_class():
+	def __init__(self):
+		print('im from constructor in my_class')
+	def __del__(self):
+		print(' im from destructor in my class')
+
+		
+
+>>> s=my_class()
+im from constructor in my_class
+
+>>> s.__del__
+<bound method my_class.__del__ of <__main__.my_class object at 0x000001D2D8C9EBB0>>
+
+>>> s.__del__()
+ im from destructor in my class
+
+>>> s
+<__main__.my_class object at 0x000001D2D8C9EBB0>
+
+>>> d=my_class()
+im from constructor in my_class
+
+>>> del d
+ im from destructor in my class
+
+
